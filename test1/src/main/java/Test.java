211611ed9ae821e166f4970f2cf955a908577485
jm_shanxuerui001 <764795838@qq.com>
2019-12-25 16:03:11 +0800

    test01

---
+++ test1/src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @Date:2019/12/25 16:01
+ * @Author:单雪瑞
+ * @Phone:15735180884
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("laokuang");
+    }
+
+}
---
+++ test1/src/main/java/Test2.java
@@ -0,0 +1,7 @@
+/**
+ * @Date:2019/12/25 16:01
+ * @Author:单雪瑞
+ * @Phone:15735180884
+ */
+public class Test2 {
+}
